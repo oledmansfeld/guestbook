21197290ff0e67b100fe7f0442adf8ee84af743c
oledmansfeld <38720025+oledmansfeld@users.noreply.github.com>
2022-07-14 11:37:10 +0200

    Update ConferenceController.php
    yep

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
@@ -11,6 +12,12 @@ class ConferenceController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
+        $greet = '';
+        if ($name = $request->query->get('hello')){
+            
+        }
+
+
         return new Response("
         <html>
             <link rel='shortcut icon' href='data:image/x-icon;,' type='image/x-icon'>
